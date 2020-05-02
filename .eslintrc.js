42bfd2ff45e97be48da19775b4fe0b808243102a
tomoya <tomoya.ton@gmail.com>
2020-05-03 00:43:53 +0900

    Setup Prettier as ESLint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,29 @@
 module.exports = {
-  extends: ['airbnb-typescript'],
+  extends: [
+    'airbnb-typescript',
+    'plugin:prettier/recommended',
+    'prettier/react',
+    'prettier/@typescript-eslint',
+  ],
   parserOptions: {
     project: './tsconfig.json',
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        semi: true, // https://github.com/airbnb/javascript#21.1
+        singleQuote: true, // https://github.com/airbnb/javascript#6.1
+        trailingComma: 'all', // https://github.com/airbnb/javascript#20.2
+        printWidth: 100, // https://github.com/airbnb/javascript#19.13
+        jsxBracketSameLine: false, // https://github.com/airbnb/javascript/tree/master/react#alignment
+        jsxSingleQuote: false, // https://github.com/airbnb/javascript/tree/master/react#quotes
+        tabWidth: 2, // https://github.com/airbnb/javascript#19.1
+        useTabs: false, // https://github.com/airbnb/javascript#19.1
+        quoteProps: 'as-needed', // https://github.com/airbnb/javascript#3.6
+        bracketSpacing: true, // https://github.com/airbnb/javascript#19.12
+        arrowParens: 'always', // https://github.com/airbnb/javascript#8.4
+      },
+    ],
   },
 };
