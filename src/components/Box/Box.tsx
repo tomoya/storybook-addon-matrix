9cb172958ca8ca5871efa89d16b85cd0f712d89d
tomoya <tomoya.ton@gmail.com>
2020-05-03 23:47:21 +0900

    Create Box component

---
+++ src/components/Box/Box.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import { space, color, layout, LayoutProps, SpaceProps, ColorProps } from 'styled-system';
+
+export type BoxProps = LayoutProps & SpaceProps & ColorProps;
+
+export const Box: React.FC<BoxProps> = styled.div`
+  ${color}
+  ${space}
+  ${layout}
+`;
