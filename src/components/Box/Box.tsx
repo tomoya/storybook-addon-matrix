76b4bf089706ffb404828ea4266996a181a4b55b
tomoya <tomoya.ton@gmail.com>
2020-05-04 00:35:11 +0900

    Add border and shadow to Box

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -1,11 +1,24 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import { space, color, layout, LayoutProps, SpaceProps, ColorProps } from 'styled-system';
+import {
+  space,
+  color,
+  layout,
+  border,
+  shadow,
+  LayoutProps,
+  SpaceProps,
+  ColorProps,
+  BorderProps,
+  ShadowProps,
+} from 'styled-system';
 
-export type BoxProps = LayoutProps & SpaceProps & ColorProps;
+export type BoxProps = LayoutProps & SpaceProps & ColorProps & BorderProps & ShadowProps;
 
 export const Box: React.FC<BoxProps> = styled.div`
   ${color}
   ${space}
   ${layout}
+  ${border}
+  ${shadow}
 `;
