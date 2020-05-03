4633d75abdc52a157ead53889cae79a809c807b0
tomoya <tomoya.ton@gmail.com>
2020-05-03 22:39:05 +0900

    Use matrix in Box story file

--- example/src/Box/Box.stories.tsx
+++ example/src/Box/Box.stories.tsx
@@ -4,6 +4,15 @@ import { Box } from './Box';
 export default {
   title: 'Box',
   component: Box,
+  parameters: {
+    matrix: {
+      pattern: {
+        bg: ['white', 'blue', 'red', 'yellow'],
+        width: [1, '50%', 256],
+        p: [1, 2, 3],
+      },
+    },
+  },
 };
 
-export const Basic = () => <Box />;
+export const Basic = () => <Box>TEXT</Box>;
