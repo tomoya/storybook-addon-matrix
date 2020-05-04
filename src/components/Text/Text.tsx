fee60804307276e189d523b9dc495d54c90d5193
tomoya <tomoya.ton@gmail.com>
2020-05-05 06:43:02 +0900

    Set font family
    Ref https://github.com/storybookjs/design-system/blob/master/src/components/shared/styles.js

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -3,8 +3,14 @@ import styled from 'styled-components';
 import { space, color, typography } from 'styled-system';
 import { TextProps } from './types';
 
-export const Text: React.FC<TextProps> = styled.span`
+const Text: React.FC<TextProps> = styled.span`
   ${space}
   ${color}
   ${typography}
 `;
+
+Text.defaultProps = {
+  fontFamily: '"Nunito Sans", "Helvetica Neue", Helvetica, Arial, sans-serif',
+};
+
+export { Text };
