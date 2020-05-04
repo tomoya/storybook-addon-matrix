5ad4a0c9ec93ab18823ba86eb4a272a73fd1bc6e
tomoya <tomoya.ton@gmail.com>
2020-05-05 02:15:45 +0900

    Use column style

--- src/components/Matrix/Matrix.tsx
+++ src/components/Matrix/Matrix.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Box } from '../Box';
 import { Text } from '../Text';
+import { Flex } from '../Flex';
 import { createPropsCombinations } from '../../createPropsCombinations';
 
 export type MatrixProps = {
@@ -12,24 +13,29 @@ export type MatrixProps = {
 export const Matrix: React.FC<MatrixProps> = ({ component, propsPattern, originalProps }) => {
   const propsCombinations = createPropsCombinations(propsPattern);
   const Component = component;
-  return propsCombinations.map((props: React.Props<any>) => {
-    const propsString = JSON.stringify(props);
-    return (
-      <Box
-        key={encodeURIComponent(propsString)}
-        border="1px solid #ddd"
-        m={2}
-        mb={3}
-        boxShadow="1px 1px 6px #eee"
-        borderRadius={3}
-      >
-        <Box bg="#eeeeee" p={2}>
-          <Text>{propsString}</Text>
-        </Box>
-        <Box m={2}>
-          <Component {...originalProps} {...props} />
-        </Box>
-      </Box>
-    );
-  });
+  return (
+    <Flex flexWrap="wrap">
+      {propsCombinations.map((props: React.Props<any>) => {
+        const propsString = JSON.stringify(props);
+        return (
+          <Flex
+            key={encodeURIComponent(propsString)}
+            border="1px solid #ddd"
+            m="auto"
+            mb={3}
+            boxShadow="1px 1px 6px #eee"
+            borderRadius={3}
+            width={3/10}
+          >
+            <Box bg="#eeeeee" p={2} width={1/5}>
+              <Text><span style={{ overflowWrap: 'break-word' }}>{propsString}</span></Text>
+            </Box>
+            <Box m={2} overflow="hidden">
+              <Component {...originalProps} {...props} />
+            </Box>
+          </Flex>
+        );
+      })}
+    </Flex>
+  );
 };
