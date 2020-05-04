ff92cddff73055e21dc521feb74a57d6802b72b9
tomoya <tomoya.ton@gmail.com>
2020-05-05 02:38:35 +0900

    Improve props display

--- src/components/Matrix/Matrix.tsx
+++ src/components/Matrix/Matrix.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-magic-numbers */
 import * as React from 'react';
 import { Box } from '../Box';
 import { Text } from '../Text';
@@ -15,27 +16,32 @@ export const Matrix: React.FC<MatrixProps> = ({ component, propsPattern, origina
   const Component = component;
   return (
     <Flex flexWrap="wrap">
-      {propsCombinations.map((props: React.Props<any>) => {
-        const propsString = JSON.stringify(props);
-        return (
-          <Flex
-            key={encodeURIComponent(propsString)}
-            border="1px solid #ddd"
-            m="auto"
-            mb={3}
-            boxShadow="1px 1px 6px #eee"
-            borderRadius={3}
-            width={3/10}
-          >
-            <Box bg="#eeeeee" p={2} width={1/5}>
-              <Text><span style={{ overflowWrap: 'break-word' }}>{propsString}</span></Text>
-            </Box>
-            <Box m={2} overflow="hidden">
-              <Component {...originalProps} {...props} />
-            </Box>
-          </Flex>
-        );
-      })}
+      {propsCombinations.map((props: React.Props<any>) => (
+        <Flex
+          key={encodeURIComponent(JSON.stringify(props))}
+          border="1px solid #ddd"
+          m="auto"
+          mb={3}
+          boxShadow="1px 1px 6px #eee"
+          borderRadius={3}
+          width={3 / 10}
+        >
+          <Box bg="#eeeeee" p={2} width={128}>
+            {Object.entries(props).map(([key, value]) => {
+              return (
+                <Box key={key}>
+                  <Box mb={2}>
+                    <Text fontWeight="bold">{key}</Text>: <Text>{value}</Text>
+                  </Box>
+                </Box>
+              );
+            })}
+          </Box>
+          <Box m={2} overflow="hidden">
+            <Component {...originalProps} {...props} />
+          </Box>
+        </Flex>
+      ))}
     </Flex>
   );
 };
